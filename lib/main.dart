803522920ad3f379044d782f7989ea901897d964
RujakBuah <farizm899@gmail.com>
2025-05-23 14:19:37 +0700

    Revamping the navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:aidminton/pages/customerService.dart';
+import 'package:aidminton/pages/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:aidminton/pages/home.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +17,131 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Gilroy'
       ),
-      home: HomePage(),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int myIndex = 1;
+
+  final List<Widget> pages = [
+    CustomerService(),
+    HomePage(),
+    Settings(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: myIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: navigationBar(),
+    );
+  }
+
+  Widget navigationBar() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 0.1, vertical: 0.1),
+        decoration: BoxDecoration(
+          color: Color(0xffCCECEE),
+          borderRadius: BorderRadius.circular(45),
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: myIndex,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Color(0xffE2FCD6),
+          unselectedItemColor: Color(0xff095D7E),
+          onTap: (index) {
+            setState(() {
+              myIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Container(
+                  width: 43,
+                  height: 43,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: myIndex == 0
+                          ? Color(0xff095D7E)
+                          : Colors.transparent),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/icons/Customer Care.svg',
+                      width: 30,
+                      height: 24,
+                      colorFilter: ColorFilter.mode(
+                        myIndex == 0 ? Color(0xffE2FCD6) : Color(0xff095D7E),
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                label: 'Customer Care'),
+            BottomNavigationBarItem(
+                icon: Container(
+                  width: 43,
+                  height: 43,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: myIndex == 1
+                          ? Color(0xff095D7E)
+                          : Colors.transparent),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/icons/Home.svg',
+                      width: 30,
+                      height: 24,
+                      colorFilter: ColorFilter.mode(
+                        myIndex == 1 ? Color(0xffE2FCD6) : Color(0xff095D7E),
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Container(
+                  width: 43,
+                  height: 43,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: myIndex == 2
+                          ? Color(0xff095D7E)
+                          : Colors.transparent),
+                  child: Center(
+                    child: SvgPicture.asset(
+                      'assets/icons/Settings.svg',
+                      width: 30,
+                      height: 24,
+                      colorFilter: ColorFilter.mode(
+                        myIndex == 2 ? Color(0xffE2FCD6) : Color(0xff095D7E),
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+                label: 'Settings'),
+          ],
+        ),
+      ),
     );
   }
 }
